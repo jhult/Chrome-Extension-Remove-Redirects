cba2b577461a288e1c2c553032177a5865a92db5
Elad Karako <eladkarako@gmail.com>
2016-08-02 05:10:20 +0300

    adding Google images redirects and anti-JSACTION events

--- resources/at_document_end.js
+++ resources/at_document_end.js
@@ -7,32 +7,28 @@
    ╚════════════════════════════════════════════════════════════════════╝
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
 
-NodeList.prototype.forEach = Array.prototype.forEach;
-(function(window, document, tmp, elements){
-
+(function(elements){
   if(null === elements || 0 === elements.length) return;
 
   /* notify chrome-extension, so the number will be shown as a "badge" */
   chrome.runtime.sendMessage({number_of_elements_with_redirects: elements.length});
 
   /* remove redirects in favor of real-link */
-  elements.forEach(function(element){
+  Array.prototype.forEach.call(elements, function(element){
     element.removeAttribute("onmousedown");
+    element.removeAttribute("jsaction");
     
     if(null === element.parentNode) return;
-    tmp = element.cloneNode(true); /* remove events */
-    element.parentNode.replaceChild(tmp, element);
+    element.parentNode.replaceChild(element.cloneNode(true), element);
   });
 }(
-  window
-, document
-, null
-, document.querySelectorAll(
+  document.querySelectorAll(
     [  '[href][onmousedown*="rwt("]'
     ,  '[href][onmousedown*=".href="]'
     ,  '[href][onmousedown*="window.open("]' 
     ,  '[href][onmousedown*="location.replace("]'
     ,  '[href][onmousedown*="location="]'
+    ,  '[href][jsaction*="mousedown"][jsaction*="keydown"]'
     ].join(', ')
   )
 ));
