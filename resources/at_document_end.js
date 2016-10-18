c0f5024523ce981b760b3d4a5139bbd7beb039bb
Elad Karako <eladkarako@users.noreply.github.com>
2016-10-18 04:24:05 +0300

    - remove the code from 'window.onload' (document_idle) in-favor of DOMContentLoaded (document_end) with 7-seconds-repeating interval. - query improving to address only high-quality "outside" links (no empty of "#" urls). - adding "onclick" removal cases (websites such as quora.com uses this old $hit :/ ) - better non-attribute event unhooking by cloneNode trick, this must come preceding to removal of the "bad-cases" attribute removal due to cloneNode restoring the original state of the node ("from HTML"), attributes included, so in-case the "bad attributes" were originated from HTML, we still need the attribute removal afterwards. - various preferences improvements. - secondary versioning advancing due to code-engine changes. minor changes due to improvements :)

--- resources/at_document_end.js
+++ resources/at_document_end.js
@@ -7,28 +7,67 @@
    ╚════════════════════════════════════════════════════════════════════╝
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
 
-(function(elements){
-  if(null === elements || 0 === elements.length) return;
+(function(setInterval){
+  "use strict";
+  
+  setInterval(function(){
 
-  /* notify chrome-extension, so the number will be shown as a "badge" */
-  chrome.runtime.sendMessage({number_of_elements_with_redirects: elements.length});
 
-  /* remove redirects in favor of real-link */
-  Array.prototype.forEach.call(elements, function(element){
-    element.removeAttribute("onmousedown");
-    element.removeAttribute("jsaction");
-    
-    if(null === element.parentNode) return;
-    element.parentNode.replaceChild(element.cloneNode(true), element);
-  });
-}(
-  document.querySelectorAll(
-    [  '[href][onmousedown*="rwt("]'
-    ,  '[href][onmousedown*=".href="]'
-    ,  '[href][onmousedown*="window.open("]' 
-    ,  '[href][onmousedown*="location.replace("]'
-    ,  '[href][onmousedown*="location="]'
-    ,  '[href][jsaction*="mousedown"][jsaction*="keydown"]'
-    ].join(', ')
-  )
-));
+    (function(elements){
+      if(null === elements || 0 === elements.length) return;
+
+      /* notify chrome-extension, so the number will be shown as a "badge" */
+      chrome.runtime.sendMessage({number_of_elements_with_redirects: elements.length});
+
+      /* remove redirects in favor of real-link */
+      Array.prototype.forEach.call(elements, function(element){
+        var tmp;
+
+        /* remove non-attribute hooks by reparsing original node's-state from HTML source. */
+        if(null !== element.parentNode){
+          tmp = element.cloneNode(true);
+          element.parentNode.replaceChild(tmp, element);
+          element = tmp; 
+        }
+
+        /* remove attribute hooks, must be second to "cloneNode" above, since if the attribute was originated from the HTML the cloneNode will restore it.. */ 
+        element.removeAttribute("onmousedown");
+        element.removeAttribute("jsaction");
+        element.removeAttribute("onclick"); /* dangerous (but required for quora and other sites..). */
+                                            /* TODO: check before each removeAttribute, if it is needed. */
+        
+      });
+    }(
+      document.querySelectorAll(
+        [
+        /* Google (generic/images search + new HTML5 hooks flag) */
+           '[href]:not([href=""]):not([href^="#"])[onmousedown*="rwt("]'
+        ,  '[href]:not([href=""]):not([href^="#"])[jsaction*="mousedown"][jsaction*="keydown"]'
+
+        /* main issue: [Google|Outbrain|Taboola] uses it (last two Co. had copied it from Google..) */
+        ,  '[href]:not([href=""]):not([href^="#"])[onmousedown*="window.open("]' 
+        ,  '[href]:not([href=""]):not([href^="#"])[onmousedown*="parent.open("]' 
+        ,  '[href]:not([href=""]):not([href^="#"])[onmousedown*="self.open("]' 
+        ,  '[href]:not([href=""]):not([href^="#"])[onmousedown*="top.open("]' 
+
+        /* generic and common way to do the same thing */
+        ,  '[href]:not([href=""]):not([href^="#"])[onmousedown*=".href="]'
+        ,  '[href]:not([href=""]):not([href^="#"])[onmousedown*="location.replace("]'
+        ,  '[href]:not([href=""]):not([href^="#"])[onmousedown*="location="]'
+
+        /* used on quora.com */
+        ,  '[href]:not([href=""]):not([href^="#"])[onclick*="openUrl("]'
+
+        /* uncommon but in use.*/
+        ,  '[href]:not([href=""]):not([href^="#"])[onclick*="window.open("]' 
+        ,  '[href]:not([href=""]):not([href^="#"])[onclick*="parent.open("]' 
+        ,  '[href]:not([href=""]):not([href^="#"])[onclick*="self.open("]' 
+        ,  '[href]:not([href=""]):not([href^="#"])[onclick*="top.open("]' 
+
+        ].join(', ')
+      )
+    ));
+
+
+  }, 7000);
+}(self.setInterval));
